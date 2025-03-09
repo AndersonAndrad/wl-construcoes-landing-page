1e7dd21d9e024a8b5f3e9b29d84df73a19e51c61
Anderson Andrade <anderson_andrade_@outlook.com>
2025-03-09 20:47:09 -0300

    refactor(change number to redirect): implement a correct number to redirect from whatsapp

--- src/core/services/whatsapp.service.ts
+++ src/core/services/whatsapp.service.ts
@@ -3,7 +3,7 @@ export interface WhatsappProps {
 }
 
 export const redirectToWhatsapp = (props?: WhatsappProps): void => {
-    const phoneNumber: string = "5561983536556";
+    const phoneNumber: string = "5561992254918";
     const message: string = props?.message ?? "Olá, gostaria de fazer um orçamento";
     const url: string = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
 
